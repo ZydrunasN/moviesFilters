d80e4daa3305531f8add09299425f2dd3ed7d72e
Zydrunas <Znorvilas28@gmail.com>
2024-07-27 14:42:20 +0300

    Added redirecting from /filter endpoint to home with model data

--- src/main/java/com/example/moviesfilters/controller/FilterController.java
+++ src/main/java/com/example/moviesfilters/controller/FilterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,11 +24,12 @@ public class FilterController {
     }
 
     @PostMapping("/filter")
-    public String filterMovies(FilterDto filterDto, Model model) {
+    public String filterMovies(FilterDto filterDto, RedirectAttributes redirectAttributes) {
         List<Movie> movies = ghibliApi.apiCall();
         var moviesFiltered = moviesService.filterByNameAndTime(movies,filterDto.getTitle(),filterDto.getRuntime()
                 ,filterDto.getRuntimeOptions());
-        model.addAttribute("filteredMovies",moviesFiltered);
-        return "index";
+        redirectAttributes.addFlashAttribute("filteredMovies", moviesFiltered);
+
+        return "redirect:";
     }
 }
--- src/main/java/com/example/moviesfilters/controller/MainController.java
+++ src/main/java/com/example/moviesfilters/controller/MainController.java
@@ -4,6 +4,8 @@ import com.example.moviesfilters.dto.FilterDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
 public class MainController {
@@ -13,5 +15,12 @@ public class MainController {
         var filterDto = new FilterDto();
         model.addAttribute("filterDto",filterDto);
         return "index";
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        if (model.containsAttribute("filteredMovies")) {
+            model.addAttribute("filteredMovies", model.asMap().get("filteredMovies"));
+        }
     }
 }
